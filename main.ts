fd5404f663aee506070ca4389a4d37551c60001e
Eric-Wang0506 <72305037+Eric-Wang0506@users.noreply.github.com>
2020-10-03 20:51:40 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else if (sprite.get(LedSpriteProperty.X) == 3 || sprite.get(LedSpriteProperty.X) == 1) {
+        game.addScore(-1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    basic.pause(500)
+})
